feaafdc990749916d1d8d1a3e33fe57829d2eac6
Rizaldy Kusuma <92089508+Rizaldy653@users.noreply.github.com>
2026-02-11 13:29:16 +0700

    Update CustomerController.php

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -19,20 +19,7 @@ class CustomerController extends Controller
 
     public function getTopSpender()
     {
-        $user = DB::table('customers')
-            ->leftJoin('orders', 'customers.customer_id', '=', 'orders.customer_id')
-            ->leftJoin('order_details', 'orders.order_id', '=', 'order_details.order_id')
-            ->paginate(10);
-        
-        $data = [
-            'company_name' => $user->company_name,
-            'country' => $user->country,
-            'total' => $user->sum('freight')
-        ];
-
-        return response()->json([
-            'message' => 'Success',
-            'data' => $data
-        ]);
+        $topSpenders = Customer::getTopSpender();
+        return response()->json($topSpenders);
     }
 }
