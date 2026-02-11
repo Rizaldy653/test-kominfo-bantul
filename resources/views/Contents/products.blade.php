95605a09a53ac8ae9de9f1f8b6709d88d95a1ecf
Rizaldy Kusuma <92089508+Rizaldy653@users.noreply.github.com>
2026-02-11 13:16:50 +0700

    Update products.blade.php

--- resources/views/Contents/products.blade.php
+++ resources/views/Contents/products.blade.php
@@ -37,7 +37,7 @@
                 processing: true,
                 ajax: {
                     url: "{{ route('products.index') }}",
-                    dataSrc: data.dats
+                    dataSrc: data.data
                 },
                 columns: [{
                         data: null,
